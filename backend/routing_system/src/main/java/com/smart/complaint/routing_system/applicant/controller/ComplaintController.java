e6bbb18b2021335414ef7794a4cf41de24a2c166
me1onsoda <bkoon1725@gmail.com>
2026-01-08 14:43:52 +0900

    api충돌 해결
    api충돌 해결

--- backend/routing_system/src/main/java/com/smart/complaint/routing_system/applicant/controller/ComplaintController.java
+++ backend/routing_system/src/main/java/com/smart/complaint/routing_system/applicant/controller/ComplaintController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/complaints")
+@RequestMapping("/api/agent/complaints")
 @RequiredArgsConstructor
 public class ComplaintController {
 
